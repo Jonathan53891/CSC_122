fb5000c3c4558bdbd2e22acf1ba2c51ada6441b0
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-09 20:05:10 -0500

    Update 2D_Arrays.cpp

--- CSC 122 Notes/Arrays & C-Strings/2D_Arrays.cpp
+++ CSC 122 Notes/Arrays & C-Strings/2D_Arrays.cpp
@@ -9,5 +9,25 @@ using namespace std;
 
 int main()
 {
+    int Matrix[3][3] = {    {   1,      2,      3   }, //row1
+                            {   4,      5,      6   }, //row2
+                            {   7,      8,      9   }  //row3
+                          //{column1,column2,column3}
+                       };
+    
+    /* Code written above is the code needed to describe a 3x3 matrix. The 
+    notation of a matrix here (and just in general), is "rows x columns". */
+    
+    /*
+    Can also be written as:
+    
+    int Matrix[3][2] = {    {   1,      2   }, //row1
+                            {   4,      5,  }, //row2
+                            {   7,      8,  }  //row3
+                          //{column1,column2}
+                       };
+    
+    */
+    
     return 0;
 }
