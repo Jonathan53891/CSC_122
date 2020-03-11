813d51f708e920af27b56a63f8a48c0db76a14ac
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-11 14:13:39 -0500

    Update balance_check.cpp

--- Portfolio 2/Projects/Balance in all things/balance_check.cpp
+++ Portfolio 2/Projects/Balance in all things/balance_check.cpp
@@ -1,1 +1,24 @@
+#include "balance_check.h"
+#include "balance_money.h"
+#include <iostream>
+
+using namespace std;
+//Function to input data into check
+void Check::input(std::istream & ins)
+{
+	char choice;
+	ins >> number;
+	amount.input(ins);
+	ins >> choice;
+	//returns true or false depending on y/n
+	if (choice == 'y')
+	{
+		cashed = true;
+	}
+	else
+	{
+		cashed = false;
+	}
+
+}
 
