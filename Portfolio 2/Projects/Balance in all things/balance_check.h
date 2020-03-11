c6a975c93b3dcee4a7a0bd32a2a62ac139f1780d
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-11 14:14:16 -0500

    Update balance_check.h

--- Portfolio 2/Projects/Balance in all things/balance_check.h
+++ Portfolio 2/Projects/Balance in all things/balance_check.h
@@ -1,1 +1,26 @@
+#ifndef BALANCE_CHECK_H
+#define BALANCE_CHECK_H
+//Check Implementation File
+#include <iostream>
+#include "balance_money.h"
+
+class Check
+{
+	//member variables
+	long number;
+	Money amount;
+	bool cashed;
+
+public:
+	// Constructor
+	Check() : number(), amount(), cashed() {}
+	//Accessors
+	long get_number() { return number; }
+	Money get_amount() { return amount; }
+	bool get_cashed() { return cashed; }
+	//Mutator
+	void input(std::istream & ins);	
+};
+
+#endif
 
