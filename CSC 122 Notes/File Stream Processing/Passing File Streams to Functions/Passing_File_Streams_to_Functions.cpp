b719eca974c1d893f3053522d95bfee6f55c9c22
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-12 01:39:17 -0500

    Create Passing_File_Streams_to_Functions.cpp

---
+++ CSC 122 Notes/File Stream Processing/Passing File Streams to Functions/Passing_File_Streams_to_Functions.cpp
@@ -0,0 +1,13 @@
+/******************************************************************************
+
+3. Passing File Streams to Functions
+
+*******************************************************************************/
+#include <fstream>
+#include "Passing_File_Streams_to_Functions.h"
+
+int main()
+{
+    ifstream in;
+    fileopen(in, "input");
+}
