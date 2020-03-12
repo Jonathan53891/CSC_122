76738e9d4cea2ebf011449603e14532226d0929b
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-12 01:27:30 -0500

    Create lose_number_templist.h

---
+++ Portfolio 2/Projects/How many items are in YOUR list/lose_number_templist.h
@@ -0,0 +1,22 @@
+#ifndef LOSE_NUMBER_TEMPLIST_H_
+#define LOSE_NUMBER_TEMPLIST_H_
+
+#include <iostream>
+using namespace std;
+
+struct TemperatureList{
+   public:
+       double* list;
+       long size;
+       long MAX_SIZE;
+       TemperatureList(int n);
+       void add_temperature(double temperature);
+       bool full() const;
+       long get_size() const;
+       double get_temperature(long position) const;
+       void output(ostream &outs) const;
+       double get_last() const;
+       void detele_last();
+};
+
+#endif
