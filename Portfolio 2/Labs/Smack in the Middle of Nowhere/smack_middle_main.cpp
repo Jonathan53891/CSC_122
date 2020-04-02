cc1beb4ea2fbf73ee894244cb5a5f10320aea546
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-04-02 17:13:32 -0500

    Update smack_middle_main.cpp

--- Portfolio 2/Labs/Smack in the Middle of Nowhere/smack_middle_main.cpp
+++ Portfolio 2/Labs/Smack in the Middle of Nowhere/smack_middle_main.cpp
@@ -1,5 +1,5 @@
-#include <fstream>
 #include <iostream>
+#include <fstream>
 #include <climits>
 #include <string>
 
@@ -7,162 +7,56 @@ using namespace std;
 
 int main(void)
 {
-	ifstream data_file;
-	string fname;
-	string line;
-	double num;
-	char ch;
+    double *pdata = nullptr;
+    size_t i, num_data;
 
-	while (true) {
+    cout << "How many values do you have?  ";
+    cin >> num_data;/*
+    while (num_data < 0)     // could check fail..?
+    {
+        cout << "\n\aInvalid size!  Get real!\n\n"
+             << "How many values do you have?  ";
+        cin >> num_data;
+    }*/
 
-		cout << "What is the name of the file?  ";
-		getline(cin, fname);
-		data_file.open(fname);
-		if (data_file.good()) {
-			break;
-		}
-	}
+    if (num_data == 0)
+    {
+        cout << "Okay, then...have a nice day!" << endl;
+    }
+    else
+    {
+        pdata = new double [num_data+1];  // extra space for sum
+        if (pdata != nullptr)
+        {
+            cout << "Enter values:  ";
+            for (i = 0; i < num_data; ++i)
+            {
+                cin >> pdata[i];
+            }
 
-	/*
-	* char-wise input with get()
-	*/
-	cout << "Plain while (get):\n";
-	data_file.get(ch);
-	while (!data_file.eof())
-	{
-		cout << "char read '" << ch << "'\n";
-		data_file.get(ch);
-	}
+            cout << "Adding values...";
+            for (i = 0, pdata[num_data] = 0.0; i < num_data; ++i)
+            {
+                pdata[num_data] += pdata[i];
+            }
 
-	data_file.clear();
-	data_file.seekg(0);
-	cout << "Hacked while (get):\n";
-	data_file.peek();
-	while (!data_file.eof())
-	{
-		data_file.get(ch);
-		cout << "char read '" << ch << "'\n";
-		data_file.peek();
-	}
+            cout << "Sum:  " << pdata[num_data] << endl
+                 << "Avg:  " << pdata[num_data]/num_data
+                 << endl;
+        }
+        else
+        {
+            cout << "\n\aUnable to allocate space for "
+                 << num_data << " values!\n\n"
+                 << "Please shut down other applications first..."
+                 << endl;
+        }
+        delete [] pdata;
+        pdata = nullptr;
+    }
 
-	data_file.clear();
-	data_file.seekg(0);
-	cout << "Plain do (get):\n";
-	do
-	{
-		data_file.get(ch);
-		cout << "char read '" << ch << "'\n";
-	} while (!data_file.eof());
+    cout << "\n*wave*  See you later!\n" << endl;
 
-	data_file.clear();
-	data_file.seekg(0);
-	cout << "Hacked do (get):\n";
-	data_file.peek();
-	do
-	{
-		data_file.get(ch);
-		cout << "char read '" << ch << "'\n";
-		data_file.peek();
-	} while (!data_file.eof());
-
-	cout << "Done with get(char) testing...\n\nPress <Enter> to continue...\n";
-	cin.ignore(INT_MAX, '\n');
-
-	/*
-	* numeric input with >>
-	*/
-	data_file.clear();
-	data_file.seekg(0);
-	cout << "Plain while (>>):\n";
-	data_file >> num;
-	while (!data_file.eof())
-	{
-		cout << "number read '" << num << "'\n";
-		data_file >> num;
-	}
-
-	data_file.clear();
-	data_file.seekg(0);
-	cout << "Hacked while (>>):\n";
-	data_file >> ws;
-	while (!data_file.eof())
-	{
-		data_file >> num >> ws;
-		cout << "number read '" << num << "'\n";
-	}
-
-	data_file.clear();
-	data_file.seekg(0);
-	cout << "Plain do (>>):\n";
-	do
-	{
-		data_file >> num;
-		if (!data_file.eof())
-		{
-			cout << "number read '" << num << "'\n";
-		}
-	} while (!data_file.eof());
-
-	data_file.clear();
-	data_file.seekg(0);
-	cout << "Hacked do (>>):\n";
-	do
-	{
-		data_file >> num >> ws;
-		cout << "number read '" << num << "'\n";
-	} while (!data_file.eof());
-
-	cout << "Done with >> testing...\n\nPress <Enter> to continue...\n";
-	cin.ignore(INT_MAX, '\n');
-
-	/*
-	* whole-line input with getline()
-	*/
-	data_file.clear();
-	data_file.seekg(0);
-	cout << "Plain while (getline):\n";
-	getline(data_file, line);
-	while (!data_file.eof())
-	{
-		cout << "line read '" << line << "'\n";
-		getline(data_file, line);
-	}
-
-	data_file.clear();
-	data_file.seekg(0);
-	cout << "Hacked while (getline):\n";
-	data_file.peek();
-	while (!data_file.eof())
-	{
-		getline(data_file, line);
-		cout << "line read '" << line << "'\n";
-		data_file.peek();
-	}
-
-	data_file.clear();
-	data_file.seekg(0);
-	cout << "Plain do (getline):\n";
-	do
-	{
-		getline(data_file, line);
-		if (!data_file.eof())
-		{
-			cout << "line read '" << line << "'\n";
-		}
-	} while (!data_file.eof());
-
-	data_file.clear();
-	data_file.seekg(0);
-	cout << "Hacked do (getline):\n";
-	do
-	{
-		getline(data_file, line);
-		cout << "line read '" << line << "'\n";
-		data_file.peek();
-	} while (!data_file.eof());
-
-	data_file.close();
-	data_file.clear();
-
-	return 0;
+    return 0;
 }
+
