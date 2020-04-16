f16739eca16e7523edcd2c133b43ba419965ed5e
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-04-16 00:01:48 -0500

    Update eof_main.cpp

--- Portfolio 3/Labs/What do you mean EOF?!/eof_main.cpp
+++ Portfolio 3/Labs/What do you mean EOF?!/eof_main.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int main()
 {
-char stringA[256];
-char stringB[256];
-  
-cout << "Enter string A:";
-cin.getline(stringA,256);
-cout << "stringA is : " << stringA << endl ;
-cout << "Enter string B: ";
-cin >> stringB;
-cout << "stringB is: " << stringB << endl ;
-  
-return 0;
+    char stringA[256];
+    char stringB[256];
+    
+    cout << "Enter string A:";
+    cin.getline(stringA,256);
+    cout << "stringA is : " << stringA << endl;
+    cout << "Enter string B: ";
+    cin >> stringB;
+    cout << "stringB is: " << stringB << endl;
+    
+    return 0;
 }
